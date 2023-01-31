e63dfcbba2fbf5bff7be18e35dc611afb059ab3f
LauritsSchou <laurits.schou@gmail.com>
2023-01-31 12:32:09 +0100

    123123

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-document.body.innerHTML += "Hello Javascript!";
-// alert("Hello Dat.js");
-console.log("Hello Dat.js");
-console.log("Hi students");
-console.log("This is your first bongobongo");
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
