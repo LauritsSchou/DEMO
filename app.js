fcc8108606b7ab9c38fa46de6ab7ccbd932d37ce
LauritsSchou <laurits.schou@gmail.com>
2023-01-31 13:37:06 +0100

    navnemaskinen

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 function sayHello() {
   document.querySelector("h3").textContent =
-    "Hej " + document.querySelector("input").value;
+    "Hej " + document.querySelector("#name").value;
 }
-document.querySelector("button").addEventListener("click", sayHello);
+function sayAge() {
+  document.querySelector("h4").textContent =
+    "Du er " + document.querySelector("#age").value + " år";
+}
+function sayEmail() {
+  document.querySelector("h5").textContent =
+    "Din email er " + document.querySelector("#email").value;
+}
+document.querySelector("#nameButton").addEventListener("click", sayHello);
+document.querySelector("#ageButton").addEventListener("click", sayAge);
+document.querySelector("#emailButton").addEventListener("click", sayEmail);
